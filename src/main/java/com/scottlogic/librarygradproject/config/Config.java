9f612759179c77c983241d87fd96d272d558dece
hrngoode <hgoode@scottlogic.com>
2019-10-08 16:15:56 +0100

    git ignoring passward properties

--- src/main/java/com/scottlogic/librarygradproject/config/Config.java
+++ src/main/java/com/scottlogic/librarygradproject/config/Config.java
@@ -54,9 +54,4 @@ public class Config {
         return new BookService(bookRepository);
     }
 
-//    @Bean("TestJpa")
-//    public JpaRepo jpaRepo(){
-//        return Mockito.mock(JpaRepo.class);
-//    }
-
 }
