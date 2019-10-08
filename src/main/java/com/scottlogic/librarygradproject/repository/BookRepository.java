06cd892dff0b3f14d4b0c4317d66c01db84cd71b
hrngoode <hgoode@scottlogic.com>
2019-10-08 13:24:22 +0100

    Merge conflicts resolved.

--- src/main/java/com/scottlogic/librarygradproject/repository/BookRepository.java
+++ src/main/java/com/scottlogic/librarygradproject/repository/BookRepository.java
@@ -2,6 +2,7 @@ package com.scottlogic.librarygradproject.repository;
 
 import com.scottlogic.librarygradproject.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
